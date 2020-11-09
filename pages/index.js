ebf2b594f26499e0b7e1ddf85c1d95184872207b
Amelie Ren <meilou@hotmail.fr>
2020-11-09 12:47:04 +0100

    commit dev

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,6 @@ export default function Home({ allPostsData }) {
 					(and occasionally designing) exceptional websites, applications, and
 					everything in between.
 				</p>
-				<p>
-					(This is a sample website - you’ll be building a site like this on{' '}
-					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-				</p>
 			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
